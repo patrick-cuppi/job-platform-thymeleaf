bc18debaa9bcb57b8bc868552bc771473fff6325
Patrick Cuppi <cuppi.patrick@gmail.com>
2025-12-15 16:06:03 -0300

    init: started the journey

---
+++ src/main/java/br/com/patrickcuppi/job_platform_thymeleaf/JobPlatformThymeleafApplication.java
@@ -0,0 +1,13 @@
+package br.com.patrickcuppi.job_platform_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPlatformThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPlatformThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/patrickcuppi/job_platform_thymeleaf/JobPlatformThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.patrickcuppi.job_platform_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPlatformThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
