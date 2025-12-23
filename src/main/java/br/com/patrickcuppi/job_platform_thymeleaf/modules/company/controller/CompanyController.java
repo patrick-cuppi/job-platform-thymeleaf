b29d0f8a958e2736f57d165a22c6b0299cdc564d
Patrick Cuppi <cuppi.patrick@gmail.com>
2025-12-23 18:17:12 -0300

    feat: create login company page

--- src/main/java/br/com/patrickcuppi/job_platform_thymeleaf/modules/company/controller/CompanyController.java
+++ src/main/java/br/com/patrickcuppi/job_platform_thymeleaf/modules/company/controller/CompanyController.java
@@ -1,16 +1,24 @@
 package br.com.patrickcuppi.job_platform_thymeleaf.modules.company.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.patrickcuppi.job_platform_thymeleaf.modules.company.dto.CreateCompanyDTO;
 import br.com.patrickcuppi.job_platform_thymeleaf.modules.company.service.CreateCompanyService;
+import br.com.patrickcuppi.job_platform_thymeleaf.modules.company.service.LoginCompanyService;
 import br.com.patrickcuppi.job_platform_thymeleaf.utils.FormatErrorMessage;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/company")
@@ -18,6 +26,9 @@ public class CompanyController {
 
   @Autowired
   private CreateCompanyService createCompanyService;
+
+  @Autowired
+  private LoginCompanyService loginCompanyService;
 
   @GetMapping("/create")
   public String create(Model model) {
@@ -38,5 +49,44 @@ public class CompanyController {
     model.addAttribute("company", createCompanyDTO);
 
     return "redirect:/company/login";
+  }
+
+  @GetMapping("/login")
+  public String login() {
+
+    return "company/login";
+  }
+
+  @PostMapping("/signIn")
+  public String signIn(RedirectAttributes redirectAttributes, HttpSession session, String username, String password) {
+
+    try {
+      var token = this.loginCompanyService.execute(username, password);
+      var grants = token
+          .getRoles()
+          .stream()
+          .map(role -> new SimpleGrantedAuthority("ROLE_" + role.toUpperCase())).toList();
+
+      UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(null, null,
+          grants);
+      authToken.setDetails(token.getAccess_token());
+
+      SecurityContextHolder.getContext().setAuthentication(authToken);
+      SecurityContext securityContext = SecurityContextHolder.getContext();
+      session.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
+      session.setAttribute("token", token);
+
+      return "redirect:/company/jobs";
+
+    } catch (HttpClientErrorException e) {
+      redirectAttributes.addFlashAttribute("error_message", "Usuário ou senha incorretos, tente novamente.");
+      return "redirect:/company/login";
+    }
+  }
+
+  @GetMapping("/jobs")
+  @PreAuthorize("hasRole('COMPANY')")
+  public String jobs() {
+    return "company/jobs";
   }
 }
--- src/main/java/br/com/patrickcuppi/job_platform_thymeleaf/security/SecurityConfig.java
+++ src/main/java/br/com/patrickcuppi/job_platform_thymeleaf/security/SecurityConfig.java
@@ -17,7 +17,9 @@ public class SecurityConfig {
           auth.requestMatchers("/candidate/login").permitAll()
               .requestMatchers("/candidate/signIn").permitAll()
               .requestMatchers("/candidate/create").permitAll()
-              .requestMatchers("/company/create").permitAll();
+              .requestMatchers("/company/create").permitAll()
+              .requestMatchers("/company/signIn").permitAll()
+              .requestMatchers("/company/login").permitAll();
           auth.anyRequest().authenticated();
         })
         .formLogin(form -> form.loginPage("/candidate/login"));
